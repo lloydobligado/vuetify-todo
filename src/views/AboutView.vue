8db227857772be3e0b3ea5715a8fc3d6f8b3f89e
lloydobligado <82791735+lloydobligado@users.noreply.github.com>
2022-08-14 15:09:20 +0800

    changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae nobis, ab veritatis pariatur asperiores voluptate molestias impedit quae nostrum voluptas saepe rerum culpa vero quaerat nesciunt distinctio praesentium excepturi. Libero.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae nobis, ab veritatis pariatur asperiores voluptate molestias impedit quae nostrum voluptas saepe rerum culpa vero quaerat nesciunt distinctio praesentium excepturi. Libero.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae nobis, ab veritatis pariatur asperiores voluptate molestias impedit quae nostrum voluptas saepe rerum culpa vero quaerat nesciunt distinctio praesentium excepturi. Libero.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae nobis, ab veritatis pariatur asperiores voluptate molestias impedit quae nostrum voluptas saepe rerum culpa vero quaerat nesciunt distinctio praesentium excepturi. Libero.</p>
   </div>
 </template>
